dd008789917d6b29fe887a7ffc78cd3fee7c7dfb
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2018-07-22 09:38:37 +0530

    Update Var_Arg_Demo.java

--- programs/Var_Arg_Demo.java
+++ programs/Var_Arg_Demo.java
@@ -1,6 +1,6 @@
 public class Var_Arg_Demo {
         public static void method1(int... a){
-            System.out.println("var-arg method");
+            System.out.println("The number of arguments passed "+a.length);
         }
         public static void main(String[] args){
             method1();
